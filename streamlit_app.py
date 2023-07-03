ab1dbcb1c1a5c9a5f51fcb5d686d95b0469dda46
Aisling Lee <aislinglee@gmail.com>
2023-07-03 21:45:36 +0000

    undo moving of chat bot components

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,19 +38,19 @@ with st.sidebar:
         button = st.button("Log Out")    
         if button:
             st.session_state["password_correct"] = False 
-        
-        for msg in st.session_state.messages:
-         st.chat_message(msg["role"]).write(msg["content"])
 
-        if prompt := st.chat_input():
-            if not openai_api_key:
-                st.info("Please add your OpenAI API key to continue.")
-                st.stop()
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-            openai.api_key = openai_api_key
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            st.chat_message("user").write(prompt)
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-            msg = response.choices[0].message
-            st.session_state.messages.append(msg)
-            st.chat_message("assistant").write(msg.content)
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
