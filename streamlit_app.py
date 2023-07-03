63fc2c5f40c90b82906f235bd4b27096d9f74598
Aisling Lee <aislinglee@gmail.com>
2023-07-03 20:10:43 +0000

    removing username leaving just passowrd auth

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,35 +13,28 @@ from hugchat.login import Login
 
 
 def check_password():
-    """Returns `True` if the user had a correct password."""
+    """Returns `True` if the user had the correct password."""
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if (
-            st.session_state["username"] in st.secrets["passwords"]
-            and st.session_state["password"]
-            == st.secrets["passwords"][st.session_state["username"]]
-        ):
+        if st.session_state["password"] == st.secrets["password"]:
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store username + password
-            del st.session_state["username"]
+            del st.session_state["password"]  # don't store password
         else:
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
-        # First run, show inputs for username + password.
-        st.text_input("Username", on_change=password_entered, key="username")
+        # First run, show input for password.
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
         # Password not correct, show input + error.
-        st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
-        st.error("😕 User not known or password incorrect")
+        st.error("😕 Password incorrect")
         return False
     else:
         # Password correct.
