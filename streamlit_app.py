6524aef7c5d3bc05e2d8ec888bda5d2b4895222f
Aisling Lee <aislinglee@gmail.com>
2023-07-03 21:44:19 +0000

    tidying up caht bot image and usage

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from hugchat.login import Login
 #API_KEY = os.environ.get("GENAI_CHATBOT_APIKEY")
 
 
-st.title("💬 Chatbot")
+st.title("🤖 Group 3 Chatbot 🤖 ")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -38,19 +38,19 @@ with st.sidebar:
         button = st.button("Log Out")    
         if button:
             st.session_state["password_correct"] = False 
+        
+        for msg in st.session_state.messages:
+         st.chat_message(msg["role"]).write(msg["content"])
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+        if prompt := st.chat_input():
+            if not openai_api_key:
+                st.info("Please add your OpenAI API key to continue.")
+                st.stop()
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    openai.api_key = openai_api_key
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg.content)
+            openai.api_key = openai_api_key
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            st.chat_message("user").write(prompt)
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+            msg = response.choices[0].message
+            st.session_state.messages.append(msg)
+            st.chat_message("assistant").write(msg.content)
